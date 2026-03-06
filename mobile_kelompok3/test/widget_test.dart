e668c20cfea562ea2632a019533a37cde0f28b59
aldihaqq03 <mochaldidhiyaulhaq@gmail.com>
2026-03-06 08:34:50 +0700

    last

--- mobile_kelompok3/test/widget_test.dart
+++ mobile_kelompok3/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobile_kel3/main.dart';
+import 'package:mobile_kel3/tugas/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
